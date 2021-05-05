ea9021393defacbd1b721c98190771f4f842872c
Jared Drake <jareddrake@Jareds-MacBook-Pro.local>
2021-05-04 21:59:26 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `yarn start`
+
+Runs the app in the development mode.<br />
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.<br />
+You will also see any lint errors in the console.
+
+This is a decentralized casino application where users are able to bet money for a number between 1 and 10 and if they are correct, they win a portion of all the ether money staked after 100 total bets
+
+Tools used:
+
+- React
+- React Router
+- Web 3
+- Infura
---
+++ build/precache-manifest.64c05b11da159b7f8faa7c5437aae318.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "242021614ffe20eb4d85de59a62b5eda",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d4d34c5c1134dd23ceae",
+    "url": "./static/css/main.19386453.chunk.css"
+  },
+  {
+    "revision": "12e5aed27ba30116d141",
+    "url": "./static/js/2.a6a6cfca.chunk.js"
+  },
+  {
+    "revision": "eefa402c39a7d49bf58197a0a7cd2ee5",
+    "url": "./static/js/2.a6a6cfca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4d34c5c1134dd23ceae",
+    "url": "./static/js/main.afbad3e4.chunk.js"
+  },
+  {
+    "revision": "2564af499ddd61272f3d",
+    "url": "./static/js/runtime-main.138d7dc6.js"
+  }
+]);
